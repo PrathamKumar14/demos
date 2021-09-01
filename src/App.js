de59674483e4082635d472a2daf40d7eb3f67131
prathamkumar14 <prathamsolera@gmail.com>
2021-09-02 02:32:24 +0530

    initial commit: Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [gender, setGender] = useState("");
+  const [imageLink, setImageLink] = useState("");
+
+  useEffect(
+    fetch(
+      "https://facial-emotion-recognition.p.rapidapi.com/cloudVision/facialEmotionRecognition?source=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1418652395119153153%2FdvMUbHmM_400x400.jpg&sourceType=url",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "x-rapidapi-host": "facial-emotion-recognition.p.rapidapi.com",
+          "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+        },
+        body: JSON.stringify({
+          source:
+            "https://pbs.twimg.com/profile_images/1418652395119153153/dvMUbHmM_400x400.jpg",
+          sourceType: "url",
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.error(err);
+      }),
+    []
+  );
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(event.target.url.value);
+    setImageLink(event.target.url.value);
+    console.log(imageLink);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Start</h1>
+      <form onSubmit={handleFormSubmit} action="">
+        {/* <label for="url">Image Link</label> */}
+        <input type="url" name="url" id="url" />
+        <input type="submit" value="Send Request" />
+      </form>
     </div>
   );
 }
