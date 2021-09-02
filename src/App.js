8f7c3359110b613502227bd4094f5a27a9a7a760
prathamkumar14 <prathamsolera@gmail.com>
2021-09-03 01:58:55 +0530

    fetch relevant data on form submit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  const [age, setAge] = useState(0);
-  const [gender, setGender] = useState("");
   const [imageLink, setImageLink] = useState("");
+  const [emotions, setEmotions] = useState([]);
 
-  useEffect(
+  function fetchData() {
     fetch(
       "https://facial-emotion-recognition.p.rapidapi.com/cloudVision/facialEmotionRecognition?source=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1418652395119153153%2FdvMUbHmM_400x400.jpg&sourceType=url",
       {
@@ -17,33 +16,39 @@ function App() {
           "x-rapidapi-key": process.env.REACT_APP_API_KEY,
         },
         body: JSON.stringify({
-          source:
-            "https://pbs.twimg.com/profile_images/1418652395119153153/dvMUbHmM_400x400.jpg",
+          source: imageLink,
           sourceType: "url",
         }),
       }
     )
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        setEmotions(data.emotions[0]);
+        console.log(emotions);
+      })
       .catch((err) => {
         console.error(err);
-      }),
-    []
-  );
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    console.log(event.target.url.value);
-    setImageLink(event.target.url.value);
-    console.log(imageLink);
+      });
   }
 
   return (
     <div>
-      <h1>Start</h1>
-      <form onSubmit={handleFormSubmit} action="">
-        {/* <label for="url">Image Link</label> */}
-        <input type="url" name="url" id="url" />
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          fetchData();
+          setImageLink("");
+        }}
+        action=""
+      >
+        <label for="url">Image Link</label>
+        <input
+          onChange={(event) => setImageLink(event.target.value)}
+          type="url"
+          name="url"
+          id="url"
+          value={imageLink}
+        />
         <input type="submit" value="Send Request" />
       </form>
     </div>
