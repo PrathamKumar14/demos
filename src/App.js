89ef935991c0672373fb5bea43987a4fdada9299
prathamkumar14 <prathamsolera@gmail.com>
2021-09-04 02:04:10 +0530

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 
 function App() {
   const [imageLink, setImageLink] = useState("");
@@ -7,31 +8,31 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   function fetchData() {
-    fetch(
-      "https://facial-emotion-recognition.p.rapidapi.com/cloudVision/facialEmotionRecognition?source=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1418652395119153153%2FdvMUbHmM_400x400.jpg&sourceType=url",
-      {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          "x-rapidapi-host": "facial-emotion-recognition.p.rapidapi.com",
-          "x-rapidapi-key": process.env.REACT_APP_API_KEY,
-        },
-        body: JSON.stringify({
-          source: imageLink,
-          sourceType: "url",
-        }),
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setEmotions(data.emotions[0]);
-        console.log(emotions);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    // fetch(
+    //   "https://facial-emotion-recognition.p.rapidapi.com/cloudVision/facialEmotionRecognition?source=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1418652395119153153%2FdvMUbHmM_400x400.jpg&sourceType=url",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "content-type": "application/json",
+    //       "x-rapidapi-host": "facial-emotion-recognition.p.rapidapi.com",
+    //       "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+    //     },
+    //     body: JSON.stringify({
+    //       source: imageLink,
+    //       sourceType: "url",
+    //     }),
+    //   }
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setEmotions(data.emotions[0]);
+    //     console.log(emotions);
+    //     setIsLoading(false);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //   });
   }
 
   return (
@@ -45,17 +46,27 @@ function App() {
         }}
         action=""
       >
-        <label for="url">Image Link</label>
         <input
           onChange={(event) => setImageLink(event.target.value)}
           type="url"
           name="url"
           id="url"
           value={imageLink}
+          className="search-input"
+          placeholder="https://..."
         />
-        <input type="submit" value="Send Request" />
+        <input className="btn" type="submit" value="Send Request" />
       </form>
-      {isLoading ? "" : <p>{emotions.joyLikelihood}</p>}
+      {isLoading ? (
+        ""
+      ) : (
+        <div className="data">
+          <p>Joy: {emotions.joyLikelihood}</p>
+          <p>Anger: {emotions.angerLikelihood}</p>
+          <p>Suprise: {emotions.surpriseLikelihood}</p>
+          <p>Sorrow: {emotions.sorrowLikelihood}</p>
+        </div>
+      )}
     </div>
   );
 }
